5f510d948e4b34613dffdd29cc9ede2109931d6b
Marko Radolovic <mradolovic@gmail.com>
2026-04-14 12:08:29 +0200

    Change some text in print from io.py

--- lib/io.py
+++ lib/io.py
@@ -43,9 +43,9 @@ def console_program_chain_io():
     if program_chain_num == 1:
         programs = [
             ("rtl_sdr",        "Do you wish to run rtl_sdr"),
-            ("retchannel4bin", "Do you wish to run retchannel4bin"),
-            ("pulsar_det_an",  "Do you wish to run pulsar_det_an"),
-            ("pul_plot",       "Do you wish to run pul_plot.py"),
+            ("RTLChannel4bin", "Do you wish to run RTLChannel4bin.c?"),
+            ("pulsar_det_an",  "Do you wish to run pulsar_det_an.c?"),
+            ("pul_plot",       "Do you wish to run pul_plot.py?"),
         ]
 
         started = False
@@ -69,25 +69,25 @@ def console_program_one_io(program_chain):
             case "rtl_sdr":
                 print("We do not currently have the code for rtl_sdr, we are working on it :(")
                 exit()
-            case "retchannel4bin":
-                res["retchannel4bin_in_file"]          = prompt_field("Enter the file path to the input data for retchannel4bin", str)
-                res["retchannel4bin_out_file"]         = prompt_field("Enter the file path you want retchannel4bin to output to", str)
-                res["retchannel4bin_clock_rate"]       = prompt_field("Enter the clock rate of your data in MHz (e.g. 2)", float)
-                res["retchannel4bin_OP_data_set_rate"] = prompt_field("Enter O/P data set rate in KHz (e.g. 1)", float)
-                res["retchannel4bin_fft_points"]       = prompt_field("Enter the number of FFT points N (e.g. 16)", int)
+            case "RTLChannel4bin":
+                res["RTLChannel4bin_in_file"]          = prompt_field("Enter the file path to the input data for RTLChannel4bin", str)
+                res["RTLChannel4bin_out_file"]         = prompt_field("Enter the file path you want RTLChannel4bin to output to", str)
+                res["RTLChannel4bin_clock_rate"]       = prompt_field("Enter the sample rate of your data in MSPS (e.g. 2)", float)
+                res["RTLChannel4bin_OP_data_set_rate"] = prompt_field("Enter O/P data set rate in kHz (e.g. 1)", float)
+                res["RTLChannel4bin_fft_points"]       = prompt_field("Enter the number of FFT points N (e.g. 16)", int)
             case "pulsar_det_an":
                 res["topo_barry_observation_time"] = prompt_field("Enter the observation time in the following format 2018-06-27T02:52:00.0", str)
                 res["topo_barry_latitude"]         = prompt_field("Enter the observation latitude in the following format 54.21", float)
                 res["topo_barry_longitude"]        = prompt_field("Enter the observation longitude in the following format -1.33", float)
 
-                if "retchannel4bin_out_file" in res:
-                    res["pulsar_det_an_in_file"] = res["retchannel4bin_out_file"]
+                if "RTLChannel4bin_out_file" in res:
+                    res["pulsar_det_an_in_file"] = res["RTLChannel4bin_out_file"]
                 else:
                     res["pulsar_det_an_in_file"] = prompt_field("Enter the path to the input .bin file", str)
 
                 res["pulsar_det_an_fft_points"]       = prompt_field("Enter the N-point FFT size (e.g. 16)", int)
                 res["pulsar_det_an_data_clock_ms"]    = prompt_field("Enter the data clock in ms (e.g. 1)", float)
-                res["pulsar_det_an_topo_period_ms"]   = prompt_field("Enter the topocentric period in ms output by Topobary (e.g. 714.50000)", float)
+                res["pulsar_det_an_topo_period_ms"]   = prompt_field("Enter the topocentric period in ms - output from TopoBary.py (e.g. 714.50000)", float)
                 res["pulsar_det_an_fold_sections"]    = prompt_field("Enter the number of fold sections (e.g. 128)", int)
                 res["pulsar_det_an_fft_bins"]         = prompt_field("Enter the number of FFT bins (e.g. 1024)", int)
                 res["pulsar_det_an_pulse_width"]      = prompt_field("Enter the ATNF pulse width (e.g. 6.5)", float)
@@ -95,8 +95,8 @@ def console_program_one_io(program_chain):
                 res["pulsar_det_an_ppm_offset"]       = prompt_field("Enter the ppm offset (e.g. 6)", float)
                 res["pulsar_det_an_threshold_sigma"]  = prompt_field("Enter the threshold sigma (e.g. 2)", float)
                 res["pulsar_det_an_ppm_range_factor"] = prompt_field("Enter the ppm range factor (e.g. 2)", float)
-                res["pulsar_det_an_rf_band_mhz"]      = prompt_field("Enter the RF band in MHz (e.g. 40)", float)
-                res["pulsar_det_an_rf_center_mhz"]    = prompt_field("Enter the RF center in MHz (e.g. 35)", float)
+                res["pulsar_det_an_rf_band_mhz"]      = prompt_field("Enter the RF bandwidth in MHz (e.g. 40)", float)
+                res["pulsar_det_an_rf_center_mhz"]    = prompt_field("Enter the RF center frequency in MHz (e.g. 35)", float)
                 res["pulsar_det_an_roll_avg"]         = prompt_field("Enter the roll average number (e.g. 0)", int)
                 res["pulsar_det_an_start_section"]    = prompt_field("Enter the start section (e.g. 0)", int)
                 res["pulsar_det_an_end_section"]      = prompt_field("Enter the end section (e.g. 127)", int)
